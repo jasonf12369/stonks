dbcde5daaaac46401b2dd842c6f87db9d429b0ce
jasonf12369 <jjfu@umich.edu>
2021-08-20 13:12:46 -0700

    Cloud and test working

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from config import *
-import json
 import alpaca_trade_api as tradeapi
-#paper trading
+import json
+import time as time_true
+import pathlib
+import pandas as pd
+from td.client import TDClient
+from td.utils import TDUtilities
 
-def stonks_high(request):
-    api = tradeapi.REST(API_KEY_ID, API_SECRET_KEY, BASE_URL)
-    account = api.get_account()
-    print(test['cash'])
-    order = api.submit_order('GOOG', 8, 'buy', 'market', 'gtc')
-    print(account)
-    return 'poop'
-    #act_dict = json.load(account)
+def testcloud(self):
+    TDSession = TDClient(
+        client_id = TD_CLIENT_ID,
+        redirect_uri = "http://192.168.0.12:8080/",
+        credentials_path = "/Users/Jason/Desktop/Code/stonks/stonks.json"
+    )
+    TDSession.validate_token()
+    TDSession.login()
+
+    msft = TDSession.get_quotes(instruments=['MSFT'])
+    return msft
  
--- stonksbot.py
+++ stonksbot.py
@@ -6,14 +6,15 @@ from config import *
 from td.client import TDClient
 from td.utils import TDUtilities
 
+def testcloud(self):
+    TDSession = TDClient(
+        client_id = TD_CLIENT_ID,
+        redirect_uri = "http://192.168.0.12:8080/",
+        credentials_path = "/Users/Jason/Desktop/Code/stonks/stonks.json"
+    )
+    TDSession.validate_token()
+    TDSession.login()
 
-TDSession = TDClient(
-    client_id = TD_CLIENT_ID,
-    redirect_uri = 'http://192.168.0.12:8080/',
-    credentials_path = '/Users/Jason/Desktop/Code/stonks/stonks.json'
-)
-
-TDSession.login()
-
-msft = TDSession.get_quotes(instruments=['MSFT'])
-print(msft)
+    msft = TDSession.get_quotes(instruments=['MSFT'])
+    print(msft)
+    return msft
